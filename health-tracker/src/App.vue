fe59a6686aa6ced3cd94890e30e9987d567d8219
Blerim Mustafa <blerimmustafa@Blerims-MacBook-Pro.local>
2025-01-14 17:36:57 +0100

    Initial commit

---
+++ health-tracker/src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="app">
+    Health Tracker
+    <router-view/>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+})
+</script>
+
+
+<style>
+</style>
